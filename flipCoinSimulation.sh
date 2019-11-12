f85ea28adfbff740f2d13dd8397ce5d0e8ed94e7
usahu998 <usahu998@gmail.com>
2019-11-12 12:27:05 +0530

    flip repeated n times and heads-tails percentage calculated

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -2,13 +2,35 @@
 
 isHeads=0
 isTails=1
+headCount=0
+tailCount=0
+mulByHundred=100
+declare -A flipResults 
+read -p "enter the number of times you want to flip the coin : " repeatCount
 
-flipAcoin=$((RANDOM%2))
-
-if [ $flipAcoin -eq $isHeads ]
-then
-	echo its heads!
-else
-	echo its tails!
-fi
-
+function flip(){
+	flipAcoin=$((RANDOM%2)) 
+   if [ $flipAcoin -eq $isHeads ]
+   then
+      flipResults["$i"]=heads
+      headCount=$(($headCount+1))
+      echo its heads!
+   else
+      flipResults["$i"]=tails
+      tailCount=$(($tailCount+1))
+      echo its tails!
+   fi
+}
+for (( i=0; $i<$repeatCount ; i++ ))
+do
+	flip 
+done
+function percentage(){
+	echo "$(( $(($1*$mulByHundred)) / $2))" 
+}
+echo all keys : ${!flipResults[@]}
+echo all data : ${flipResults[@]}
+headPerc="$( percentage $headCount $repeatCount)"
+tailPerc=$(( $(($tailCount * $mulByHundred)) / $repeatCount ))
+echo heads percentage : $headPerc
+echo tails percentage : $tailPerc
