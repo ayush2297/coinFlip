bed988d8566512c7420c5999fde83bf61e182607
usahu998 <usahu998@gmail.com>
2019-11-13 18:10:43 +0530

    sorting alogrithm implemented to the generic code

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,60 +1,44 @@
 #!/bin/bash  
 
-echo "welcome"
-
 declare -A keyArr
 declare -A valArr
 declare -A result
 
 mulByHundred=100
-headCount=0
-tailCount=0
-tailTail=0
-headHead=0
-tailHead=0
-HeadTail=0
-
 
 function percentage(){
-   echo "$(( $(($1*$mulByHundred)) / $2))"
+  echo "$(( $(($1*$mulByHundred)) / $2))"
 }
 
 
 function coinFlipSimulation() {
-	for (( i=1; i<=$1; i++ ))
-	do
-		s=""
-	  	for(( j=0; j<$2; j++ ))
-	  	do
-	     	coinFlip=$(( RANDOM % 2 ))
-	      if [ $coinFlip == 1 ]
-			then
-    			s=$s"H"
-        		headCount=$(( $headCount + 1 ))
-  			else
-            s=$s"T"
-         	tailCount=$(( $tailCount + 1))
-			fi
- 		done
-		echo final key : $s
-		result["$s"]=$(( ${result["$s"]} + 1 ))  
-	done
-	analyze
+for (( i=1; i<=$1; i++ ))
+do
+s=""
+  for(( j=0; j<$2; j++ ))
+  do
+    coinFlip=$(( RANDOM % 2 ))
+     if [ $coinFlip == 1 ]
+then
+    s=$s"H"
+        headCount=$(( $headCount + 1 ))
+  else
+           s=$s"T"
+        tailCount=$(( $tailCount + 1))
+fi
+done
+echo final key : $s
+result["$s"]=$(( ${result["$s"]} + 1 ))  
+done
 }
 
-#read -p "how much time you flip coin " repititions
-#read -p "number of coins? : " numberOfCoins
-for( numberOfCoins=1 ; $numberOfCoins<4 ; numberOfCoins++ ))
-do
-	coinFlipSimulation $repititions $numberOfCoins
-done
 
-function analyze(){
-
+function displayData()
+{
 	echo ${!result[@]}
 	echo ${result[@]}
-	elCount=${#result[@]} 
-
+	elCount=${#result[@]}
+	
 	index=0
 	for key in  ${!result[@]}
 	do
@@ -62,13 +46,27 @@ function analyze(){
 		valArr[$index]=${result[$key]}
 		index=$(($index+1))
 	done
-	echo key arr before sorting : ${keyArr[@]}
+	
+	echo "key arr::::::::::::: ${keyArr[@]}"
+	echo "val arr::::::::::::: ${valArr[@]}"
+	for (( ind=0 ; $ind<$elCount ;ind++ ))
+	do
+		echo -e "$ind	${keyArr[$ind]}	${valArr[$ind]}"
+	done
+	
+
+	for (( ind=0 ; $ind<$elCount ;ind++ ))
+	do
+	  perc="$( percentage ${valArr[$ind]} $repititions )"
+	  echo "${keyArr[$ind]}	%	=	$perc"	
+	done
+		echo key arr before sorting : ${keyArr[@]}
 	echo val arr before sorting : ${valArr[@]}
-	for ((i = 0; i<elCount; i++))
+	for ((i = 0; i<elCount; i++))	
 	do
 		jCount=$(( $elCount - 1 ))
-		for ((j = 0; $j<$jCount; j++))
-		do
+		for (( j=0 ; $j < $jCount ; j++ ))
+		do	
 			k=$(($j+1))
 			num1="${valArr[$j]}"
 			num2="${valArr[$k]}"
@@ -79,23 +77,24 @@ function analyze(){
 				valArr[$k]=$temp
 	
 				temp1=${keyArr[$j]}
-      	   keyArr[$j]=${keyArr[$k]}
-      	   keyArr[$k]=$temp1
-			fi
+	     	keyArr[$j]=${keyArr[$k]}
+	      	keyArr[$k]=$temp1
+		fi
 		done
 	done
-
-
 	for (( ind=0 ; $ind<$elCount ;ind++ ))
 	do
 		echo $ind - ${keyArr[$ind]} - ${valArr[$ind]}
 	done
-
-
-	for (( ind=0 ; $ind<$elCount ;ind++ ))
-	do
-	   perc="$( percentage ${valArr[$ind]} $elCount )"
-	   echo ${keyArr[$ind]} %  =  $perc
-	done
-
 }
+
+read -p "how much time you flip coin " repititions
+#read -p "number of coins? : " numberOfCoins
+for (( coins=1 ; $coins<4 ; coins++ ))
+do
+echo "*************COMBINATIONS FOR $coins COINS************************"
+coinFlipSimulation $repititions $coins
+echo "*************DISPLAYING PERCENTAGES************************"
+displayData
+done
+
